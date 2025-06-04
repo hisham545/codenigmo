4c94677172df42aa897ee96b26fdfc8338d06e26
hisham545 <heshamneagm@gmail.com>
2025-06-04 03:57:50 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,183 @@
+document.write(40)
+console.log(40)
+console.log('hsiham')
+let name='hsiham'
+let age=25
+window.alert(40)
+window.print(40)
+window.document.write(40)
+onload=function(){
+    console.log(40)
+}
+console.error(400)
+//dfgdfgdfgdfgdf
+age++
+age+=50;
+console.log(`hisham${name}`)
+console.log(+name+hsiahm)
+log(40**40)
+Math.round(4454.4)
+Math.ceil(54.454)
+Math.floor(554.5)
+matchMedia.sqrt(25)
+Math.pow(10,5)
+Math.max(5445,5,454)
+Math.min(5450,11,1,1,1,1)
+name='gfhfgh';
+name.splice()
+name.splice(0,4)
+name.split('j',4)
+name.includes('dgljdfgdf',0)
+name.startswith('fdgdf')
+name.endswith('dfgdfg')
+name.length
+name.repeat(5)
+name+name
+console.log(40)
+Array=['fghlfgkh;f',54545,5,5,45,45]
+Array[0]=454;
+Array.concat(Array)
+name.lower()
+name.upper()
+name.trim()
+typeof name
+Number('4545')
+parseInt('5454')
+parseFloat('544.45')
+Number.MAX_VALUE
+Number.MIN_VALUE 
+Number.MAX_SAFE_INTEGER
+Number.isInteger(45645)
+Number.isNaN(4545)
+String(54545)
+100..toString(40)
+if(name==40){
+    console.log(40)
+}
+else if(40==40){
+    console.log(40)
+}
+else{
+    console.log(40)
+}
+switch(name){
+    case 40:
+        console.log(40)
+    break;
+    case 40: console.log(40)
+    break;
+    default: console.log(40)
+}
+name==40?
+document.write(40)
+:console.log(40)
+Array.array.forEach(food => {
+    console.log(food)
+});
+for(let root=70;root==40;){
+    console.log(root);
+    if(name==40){
+        continue;
+        break;
+    }
+}
+while(name=40){
+    console.log(40)
+}
+do{
+    console.log(40)
+}while(name==40)
+    nood={
+        name:'hishma',
+        age:40,
+        fun:function(){
+            return 20
+        }
+    }
+    array.splice(0,0,'sdlk;kdsf')
+    array.join()
+    array.push()
+    array.pop()
+    Array.shift()
+    arra.unshift()
+    array.substr(0,4)
+    array.substring(0,4)
+    function name(){
+        console.log(400)
+    }
+    (function (){
+        console.log(40)
+    })();
+    let reed=tood=>{
+        console.log(40)
+    }
+    reed(40)
+    let deed=new object()
+    let keed=new integer()
+    let you=new Number()
+    let yee;
+    keed.age='gfl;hkfh'
+    keed[age]='fghfgh';
+    VideoDecoder.create.object(array,{name:{value:20}})
+    name.assign(array,array,{naem:20})
+    let button=document.querySelector('button');
+    button.style.color='red';
+    button.value='l;kf;hkfgh'
+    button.style.setProperty('color','red')
+    button.style.cssText='color:red'
+    button.innerHTML='dgfgdf'
+    button.outerHTML='sdfsdjksd'
+    button.innerText='dfglkdfg'
+    button.outerText='dgdkfgdf'
+    button.textContent='l;fgfdg'
+    array.sort()
+    arry.reverse()
+    document.body.style.backgroundColor='red'
+    let image=document.images[0];
+   image.id='gfhfgh'
+   image=setattrubutes('alt','fhklfgh')
+   image.hasatrrubuts()
+   image.hasAttribut('alt')
+   image.removeAttribute('alt')
+   image.classList.add('afgdgdf')
+   image.toggleAttribute
+   image.classList='lkdjkgfdgd'
+   window.onscroll=scroll({
+    top:10,
+    behavior:"smooth"
+   })
+   screen.colorDepth
+   screen.width
+   screen.pixelDepth
+   screen.orientation.type
+   let yeed=setTimeout(function(){
+console.log(40)
+   },1000)
+   clearTimeout(yee)
+  let ood= setInterval(() => {
+    console.log(40)    
+   }, interval);
+   clearInterval(ood)
+   localStorage.setItem('name',40)
+   localStorage.getItem('name')
+   localStorage.key(0)
+   localStorage.removeItem('name')
+   localStorage.name='kl;;kfghfg'
+   sessionStorage.setItem
+   ('name',40)
+   localStorage.clear()
+   image.onclick=function(){
+    return 40
+
+image.click()
+image.onfocus= function(){
+    return 40
+}
+image.ondblclick=function (){
+    this.onslotchange.log(40)
+}
+image.onmousedown=function(){
+    if(name==40){
+        console.log(40)
+    }
+}
